eb7f0a81364c357c4b9000a36d4cac1ca5ed5cc0
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-02-11 17:26:22 +0200

    select answers

--- src/Components/Quiz/Quiz.tsx
+++ src/Components/Quiz/Quiz.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 function Quiz() {
   let [count, SetCount] = useState(1);
   let [show, SetShow] = useState(true);
+  let [imageLoaded, SetImageLoaded] = useState(false);
 
   let subset = [...Array(4).keys()].map(() => random(cakesList));
   let [answer, setAnswer] = useState(subset[0]);
@@ -21,8 +22,8 @@ function Quiz() {
 
           {show ? (
             <div id="quiz-content">
-              <img src={answer.img} alt="" />
-              <div>
+              <img src={answer.img} alt="" onLoad={ () => SetImageLoaded(true)}/>
+              <div style={{pointerEvents: imageLoaded ? "auto" : "none"}}>
                 <Question count={count} />
                 <Choices
                   answer={answer.name}
@@ -37,6 +38,7 @@ function Quiz() {
                       setAnswer(newSubset[0])
                       setShuffled(shuffle(newSubset));
                       SetShow(true);
+                      SetImageLoaded(false);
                     }, 1000);
                   }}
                 />
