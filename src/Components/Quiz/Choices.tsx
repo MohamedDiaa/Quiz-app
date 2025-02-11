37b19c8cc990e87e77f768b1b4a4035ce23ed670
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-02-11 16:02:30 +0200

    show next question

--- src/Components/Quiz/Choices.tsx
+++ src/Components/Quiz/Choices.tsx
@@ -1,19 +1,48 @@
+import { MouseEventHandler, useState } from "react";
 import "./Choices.css";
 
 interface Props {
-
-  options: {name: string, img: string}[]
+  options: { name: string; img: string }[];
+  onAnswered: () => void
 }
 
-function Choices({options}:Props) {
+function Choices({ options, onAnswered }: Props) {
+  // first option is the correct answer
+  // we shuffle them
+  const [answered, setAnswered] = useState(-1);
 
+  let answer = options[0];
+  let shuffled = shuffle(options);
   return (
     <ul>
-      {options.map((option, index) => {
-        return <li className="choice">{option.name}</li>;
+      {shuffled.map((option, index) => {
+        return (
+          <li
+            className="choice"
+            style={
+              answered == -1
+                ? { backgroundColor: "white" }
+                : option.name == answer.name
+                ? { backgroundColor: "green", color: "white" }
+                : { backgroundColor: "red", color: "white" }
+            }
+            onClick={() => {
+              setAnswered(index);
+              setTimeout(() => {
+                onAnswered()
+              }, 1000);
+            }}
+          >
+            {option.name}
+          </li>
+        );
       })}
     </ul>
   );
 }
 
 export default Choices;
+
+function shuffle(array: { name: string; img: string }[]) {
+  return array.sort(() => Math.random() - 0.5);
+}
--- src/Components/Quiz/Quiz.tsx
+++ src/Components/Quiz/Quiz.tsx
@@ -1,30 +1,42 @@
 import Choices from "./Choices";
 import Question from "./Question";
 import cakesList from "../../json/cakes";
+import { useState } from "react";
 
 function Quiz() {
+  const [count, SetCount] = useState(1);
 
-  let subset = [1,2,3,4].map(() => random(cakesList))
+  let subset = [1, 2, 3, 4].map(() => random(cakesList));
 
   return (
     <div id="quiz">
-      <h1 id="quiz-title">Quiz</h1>
-      <div id="quiz-content">
-        <img
-          src= {subset[0].img}
-          alt=""
-        />
-        <div>
-          <Question />
-          <Choices options={subset} />
-        </div>
-      </div>
+      {count > 3 ? (
+        <h1>Quiz is finished</h1>
+      ) : (
+        <>
+          <h1 id="quiz-title">Quiz {count}</h1>
+          <div id="quiz-content">
+            <img src={subset[0].img} alt="" />
+            <div>
+              <Question />
+              <Choices
+                options={subset}
+                onAnswered={() => {
+                  SetCount(count + 1);
+                }}
+              />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
 
 export default Quiz;
 
- function random(array:  { name: string, country: string, description: string, img: string  }[]) {
-  return array[Math.floor((Math.random()*array.length))];
+function random(
+  array: { name: string; country: string; description: string; img: string }[]
+) {
+  return array[Math.floor(Math.random() * array.length)];
 }
